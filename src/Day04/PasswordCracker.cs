e11756c3d9e0dd6b0b819d299ac32594b3e88eaa
Michael Gasparelli <mgasparelli@sircorp.com>
2019-12-04 18:17:42 -0500

    Solve Day4 Part2

--- src/Day04/PasswordCracker.cs
+++ src/Day04/PasswordCracker.cs
@@ -1,29 +1,43 @@
+using System;
 using System.Collections.Generic;
 
 namespace AdventOfCode2019
 {
     public class PasswordCracker
     {
-        public static IEnumerable<string> FindAllPasswordsInRange(int min, int max)
+        public static IEnumerable<string> FindAllPasswordsInRange(int min, int max, bool strict = false)
         {
             for (int i = min; i < max; i++)
             {
-                if(MeetsRequirements(i.ToString()))
+                if(MeetsRequirements(i.ToString(), strict))
                 {
                     yield return i.ToString();
                 }
             }
         }
 
-        public static bool MeetsRequirements(string password)
+        public static bool MeetsRequirements(string password, bool strict = false)
         {
             if(password.Length != 6)
             {
                 return false;
             }
 
-            bool hasAdjacentPair = false;
+            if(!SequenceNeverDecreases(password))
+            {
+                return false;
+            }
 
+            if(strict)
+            {
+                return SequenceContainsPairStrict(password);
+            }
+
+            return SequenceContainsPair(password);
+        }
+
+        static bool SequenceNeverDecreases(string password)
+        {
             for (int i = 0; i < password.Length; i++)
             {
                 if(i == 0)
@@ -35,14 +49,55 @@ namespace AdventOfCode2019
                 {
                     return false;
                 }
+            }
 
-                if(!hasAdjacentPair && password[i] == password[i - 1])
+            return true;
+        }
+
+        static bool SequenceContainsPairStrict(string password)
+        {
+            int consecutiveDigits = 1;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if(i == 0)
                 {
-                    hasAdjacentPair = true;
+                    continue;
+                }
+
+                if(password[i] == password[i - 1])
+                {
+                    consecutiveDigits++;
+                }
+                else
+                {
+                    if(consecutiveDigits == 2)
+                    {
+                        return true;
+                    }
+
+                    consecutiveDigits = 1;
                 }
             }
 
-            return hasAdjacentPair;
+            return consecutiveDigits == 2;
+        }
+
+        static bool SequenceContainsPair(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if(i == 0)
+                {
+                    continue;
+                }
+
+                if(password[i] == password[i - 1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
--- tests/Day04Tests.cs
+++ tests/Day04Tests.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using AdventOfCode2019.Day01;
 using Xunit;
 
 namespace AdventOfCode2019.Tests
@@ -14,6 +16,15 @@ namespace AdventOfCode2019.Tests
             Assert.Equal(expected, PasswordCracker.MeetsRequirements(input));
         }
 
+        [Theory]
+        [InlineData("112233", true)]
+        [InlineData("123444", false)]
+        [InlineData("111122", true)]
+        public void Part2(string input, bool expected)
+        {
+            Assert.Equal(expected, PasswordCracker.MeetsRequirements(input, strict: true));
+        }
+
         [Fact]
         public void SolvePart1()
         {
@@ -24,7 +35,21 @@ namespace AdventOfCode2019.Tests
 
             int numPasswords = PasswordCracker.FindAllPasswordsInRange(range[0], range[1]).Count();
 
-            Assert.Equal(1, numPasswords);
+            Assert.Equal(1640, numPasswords);
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            var range = System.IO.File.ReadAllText("../../../input/day_04.txt")
+                .Split('-')
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            int numPasswords = PasswordCracker
+                .FindAllPasswordsInRange(range[0], range[1], strict: true).Count();
+
+            Assert.Equal(1126, numPasswords);
         }
     }
 }
